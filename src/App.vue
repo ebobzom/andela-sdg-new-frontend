ec451d790b185abee1ff136311ec4ea65faf2bfb
Elijah Bobzom <ebobzom@gmail.com>
2020-04-13 06:46:57 +0100

    vue frontend

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" role="main-app">
+    <div class="container" role="container">
+      <h1>Covid-19 Estimator</h1>
+      <form action="#" method="POST">
+          <label for="population">Population</label>
+          <input data-population type="text" id="population" placeholder="population" aria-label="population">
+
+          <label for="timeToElapse">Time To Elapse</label>
+          <input data-time-to-elapse type="number" id="timeToElapse" placeholder="timeToElapse" aria-label="timeToElapse">
+
+          <label for="reportedCases">Reported Cases</label>
+          <input data-reported-cases type="number" id="reportedCases" placeholder="reportedCases" aria-label="reportedCases">
+
+          <label for="totalHospitalBeds">Total Hospital Beds</label>
+          <input data-total-hospital-beds type="number" id="totalHospitalBeds" placeholder="totalHospitalBeds" aria-label="totalHospitalBeds">
+
+          <label for="periodType">Period Type</label>
+          <select data-period-type name="periodType" id="periodType">
+              <option value="days">Days</option>
+              <option value="weeks">Weeks</option>
+              <option value="months">Months</option>
+          </select>
+          <button data-go-estimate>Submit</button>
+      </form>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+    background-color: #f5f5f5;
+    font-size: 1.5em;
+}
+
+h1{
+    text-align: center;
+}
+
+form{
+    width: 40%;
+    margin: 0 auto;
+    border: 1px solid grey;
+    border-radius: 10px;
+}
+
+
+input, label{
+    display: block;
+    margin: 4px auto;
+    width: 80%;
+    height: 30px;
+    border-radius: 4px;
+}
+
+label{
+    padding: 2px;
+    margin-top: 2px;
+    font-style: italic;
+    font-weight: 600;
+}
+button, select{
+    height: 30px;
+    border-radius: 4px;
+    display: block;
+}
+
+label[for='periodType']{
+    display: inline-block;
+    text-align: center;
+    padding: 10px;
+}
+
+select{
+    text-align: center;
+    width: 30%;
+    text-align: center;
+    margin: 0 auto;
+}
+
+button{
+    width: 30%;
+    margin: 0 auto;
+    height: 35px;
+    margin-top: 20px;
+    background-color: rgb(165, 209, 165);
+    font-weight: bold;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
